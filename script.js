704a41fff7bf7b3b3580102f52f7afbba222111a
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-21 10:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function prom(params) {
+	return new Promise((res, rej)=>{
+		setTimeout(()=>{
+			res("Hello, world!")
+		},1000)
+	}) 
+}
+prom().then(d=>output.innerText=d)
